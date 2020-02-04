c420480b40f160c9aa7f972afcd4ac92324fd748
Michelle Morin <morin2m@gmail.com>
2020-02-03 19:16:06 -0800

    remove img import

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import { Board } from "./memory";
 import './styles.css';
-// import { bambi } from './img/bambi.jpeg';
-
-// var imgDeer = document.getElementById("a");
-// imgDeer.src = bambi;
 
 $(document).ready(function() {
   var board = new Board();
